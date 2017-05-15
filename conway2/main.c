d3da7ad50a7b8b02a0d801eb5873a9763385de1a
Nathanial Schaffner <schaffna@nathanials-macbook-pro.local>
2017-05-14 22:15:20 -0400

    Initial Commit

---
+++ conway2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  conway2
+//
+//  Created by Nathanial Schaffner on 5/14/17.
+//  Copyright © 2017 Nathanial Schaffner. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
